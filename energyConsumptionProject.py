e9553d3eee7540859b1762d84e9a32a9f675cc77
Anvay Panguluri <aspanguluri@gmail.com>
2023-05-22 19:14:05 -0700

    Cleaned up comments, refactor
    deleted all commented out code, refactored data files name

--- energyConsumptionProject.py
+++ energyConsumptionProject.py
@@ -1,61 +1,56 @@
-#name of venv: seng2021
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 import datetime
 
-file_data_2016 = open("data_2016.csv", "r")
-file_data_2017 = open("data_2017.csv", "r")
-file_data_2018 = open("data_2018.csv", "r")
-file_data_2019 = open("data_2019.csv", "r")
+file_data_2016 = open("data/data_2016.csv", "r")
+file_data_2017 = open("data/data_2017.csv", "r")
+file_data_2018 = open("data/data_2018.csv", "r")
+file_data_2019 = open("data/data_2019.csv", "r")
+file_data = [file_data_2016, file_data_2017, file_data_2018, file_data_2019]
 
-data_2016 = file_data_2016.readlines()[1:] #get rid of the headers line
+# get rid of the headers line
+data_2016 = file_data_2016.readlines()[1:]
 data_2017 = file_data_2017.readlines()[1:]
 data_2018 = file_data_2018.readlines()[1:]
 data_2019 = file_data_2019.readlines()[1:]
 
-total_data = data_2016+data_2017+data_2018+data_2019
-# print(total_data[:370])
+total_data = data_2016 + data_2017 + data_2018 + data_2019
 
-# reduced_total_data = [line.strip("\n").split(",")[1:2] + line.strip("\n").split(",")[3:] for line in total_data] #getting rid of 'pacific' and 'cal'
-reduced_total_data = [line.strip("\n").replace("/", ",").replace("\"", "").split(",")[1:4] + line.strip("\n").replace("/", ",").split(",")[5:-2] for line in total_data] #getting rid of 'everything but the data, demand, and demand forecast
+# getting rid of 'everything but the data, demand, and demand forecast
+reduced_total_data = [line.strip("\n").replace("/", ",").replace("\"", "").split(",")[1:4] + \
+                      line.strip("\n").replace("/", ",").split(",")[5:-2] for line in total_data]
 
 cleaned_total_data = [[int(val) for val in line] for line in reduced_total_data]
 
-file_data_2016.close()
-file_data_2017.close()
-file_data_2018.close()
-file_data_2019.close()
-
-print(reduced_total_data[1])
-print(cleaned_total_data[1])
+for f_data in file_data:
+	f_data.close()
 
 # [month, date, day, demand, demand forecast]
-for i in range(0,len(cleaned_total_data)):
-	cleaned_total_data[i][2] = datetime.date(cleaned_total_data[i][2], cleaned_total_data[i][0], cleaned_total_data[i][1]).weekday() #replacing the (now useless) year with the number corresponding to the day
+for i in range(0, len(cleaned_total_data)):
+    cleaned_total_data[i][2] = datetime.date(cleaned_total_data[i][2], cleaned_total_data[i][0], cleaned_total_data[i][
+        1]).weekday()  # replacing the (now useless) year with the number corresponding to the day
 
-	cleaned_total_data[i][0] = (cleaned_total_data[i][0]-1)/11 #resizing the data
-	cleaned_total_data[i][1] = (cleaned_total_data[i][1]-1)/30
-	cleaned_total_data[i][2] = (cleaned_total_data[i][2])/6
-	cleaned_total_data[i][3] = (cleaned_total_data[i][3]-1)/1300000
+    cleaned_total_data[i][0] = (cleaned_total_data[i][0] - 1) / 11  # resizing the data
+    cleaned_total_data[i][1] = (cleaned_total_data[i][1] - 1) / 30
+    cleaned_total_data[i][2] = (cleaned_total_data[i][2]) / 6
+    cleaned_total_data[i][3] = (cleaned_total_data[i][3] - 1) / 1300000
 
 
 class NeuralNetwork(nn.Module):
-	def __init__(self):
-		super(NeuralNetwork, self).__init__()
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
 
+        self.hiddenLayer = nn.Linear(3,128)  # month (0-11), 0-1 scaled; day of month (0-30) 0-1 scaled; day of week (0-6), 0-1 scaled
+        self.outputLayer = nn.Linear(128, 1)  # first output demand, second output est. generation required
 
-		self.hiddenLayer = nn.Linear(3, 128) # month (0-11), 0-1 scaled; day of month (0-30) 0-1 scaled; day of week (0-6), 0-1 scaled
-		self.outputLayer = nn.Linear(128, 1) #first output demand, second output est. generation required
+    def forward(self, x):
+        x = torch.sigmoid(self.hiddenLayer(x))  # hidden layer with activation
+        x = torch.sigmoid(self.outputLayer(x))  # output layer with activation
 
+        return x
 
-	def forward(self, x):
-		x = torch.sigmoid(self.hiddenLayer(x)) #hidden layer with activation
-		x = torch.sigmoid(self.outputLayer(x)) #output layer with activation
-
-		return x
 
 '''
 #Training the network
@@ -63,83 +58,72 @@ class NeuralNetwork(nn.Module):
 
 # days = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
 
-# print(cleaned_total_data[1])
-
-
-# print(cleaned_total_data[1])
-
-input_data = torch.tensor([line[0:3] for line in cleaned_total_data]) # input data
-output_data = torch.tensor([[float(line[3])] for line in cleaned_total_data]) # output data
+input_data = torch.tensor([line[0:3] for line in cleaned_total_data])  # input data
+output_data = torch.tensor([[float(line[3])] for line in cleaned_total_data])  # output data
 
 '''
 # get data for time, temperature, time of year in some big city
 # set the lower end of output to be 0, higher end to be 1
 # the optimal number should be close to 0 (means low electricity consumption)
 '''
-model = NeuralNetwork() # create the neural network; 'model'
+model = NeuralNetwork()  # create the neural network; 'model'
 
-lr = .01 # set the learning rate
-n_epochs = 50 # an epoch is one cycle through all the training data
+lr = .01  # set the learning rate
+n_epochs = 50  # an epoch is one cycle through all the training data
 
-loss_fn = nn.MSELoss() # Use mean squared error
+loss_fn = nn.MSELoss()  # Use mean squared error
 
-optimizer = optim.SGD(model.parameters(), lr=lr) # use SGD as the optimizer
+optimizer = optim.SGD(model.parameters(), lr=lr)  # use SGD as the optimizer
 
-model.train() # set the neural network into training mode
+model.train()  # set the neural network into training mode
 print("training...")
 for epoch in range(n_epochs):
-	for i in range(input_data.shape[0]): # for each training example
-		prediction = model(input_data[i])
+    for i in range(input_data.shape[0]):  # for each training example
+        prediction = model(input_data[i])
 
-		loss = loss_fn(output_data[i], prediction) # compute the loss (the error)
+        loss = loss_fn(output_data[i], prediction)  # compute the loss (the error)
 
-		# update the weights
-		loss.backward()
-		optimizer.step()
-		optimizer.zero_grad()
+        # update the weights
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-print ("Expected:")
-print (output_data)
-print ("Network output:")
-print (model(input_data))
-model.eval() #does not update any weights; for inferencing
-# torch.save(model, "model_configuration.pt")
-#to initialize a model w/ configuration
+print("Expected:")
+print(output_data)
+print("Network output:")
+print(model(input_data))
+model.eval()  # does not update any weights; for inferencing
 
-# model = torch.load("model_configuration.pt")
+# to initialize a model w/ configuration
 
-file_data_2020 = open("data_2020.csv", "r")
+file_data_2020 = open("data/data_2020.csv", "r")
 data_2020 = file_data_2020.readlines()[1:]
 
-
-reduced_data_2020 = [line.strip("\n").replace("/", ",").replace("\"", "").split(",")[1:4] + line.strip("\n").replace("/", ",").split(",")[5:-2] for line in data_2020] #getting rid of 'everything but the data, demand, and demand forecast
+reduced_data_2020 = [
+    line.strip("\n").replace("/", ",").replace("\"", "").split(",")[1:4] + line.strip("\n").replace("/", ",").split(
+        ",")[5:-2] for line in data_2020]  # getting rid of 'everything but the data, demand, and demand forecast
 
 cleaned_data_2020 = [[int(val) for val in line] for line in reduced_data_2020]
 
-for i in range(0,len(cleaned_data_2020)):
-	cleaned_data_2020[i][2] = datetime.date(cleaned_data_2020[i][2], cleaned_data_2020[i][0], cleaned_data_2020[i][1]).weekday() #replacing the (now useless) year with the number corresponding to the day
+for i in range(0, len(cleaned_data_2020)):
+    cleaned_data_2020[i][2] = datetime.date(cleaned_data_2020[i][2], cleaned_data_2020[i][0], cleaned_data_2020[i][
+        1]).weekday()  # replacing the (now useless) year with the number corresponding to the day
 
-	cleaned_data_2020[i][0] = (cleaned_data_2020[i][0]-1)/11 #resizing the data
-	cleaned_data_2020[i][1] = (cleaned_data_2020[i][1]-1)/30
-	cleaned_data_2020[i][2] = (cleaned_data_2020[i][2])/6
-	cleaned_data_2020[i][3] = (cleaned_data_2020[i][3]-1)/1300000
+    cleaned_data_2020[i][0] = (cleaned_data_2020[i][0] - 1) / 11  # resizing the data
+    cleaned_data_2020[i][1] = (cleaned_data_2020[i][1] - 1) / 30
+    cleaned_data_2020[i][2] = (cleaned_data_2020[i][2]) / 6
+    cleaned_data_2020[i][3] = (cleaned_data_2020[i][3] - 1) / 1300000
 
 fout = open("predictionsVsActual.txt", "w")
 fout.write("My prediction, official prediction, actual demand\n")
 
-
 # [month, date, day, demand, demand forecast]
 
-for i in range(0,len(cleaned_data_2020)):
-	s = ""
+for i in range(0, len(cleaned_data_2020)):
+    s = ""
 
-	s+=(str(float(model(torch.tensor(cleaned_data_2020[i][0:3]))[0])*1300000) + ", ")
-	s+=(str(cleaned_data_2020[i][4])+", ")
-	s+=str(cleaned_data_2020[i][3]*1300000)
-	fout.write(s+"\n")
+    s += (str(float(model(torch.tensor(cleaned_data_2020[i][0:3]))[0]) * 1300000) + ", ")
+    s += (str(cleaned_data_2020[i][4]) + ", ")
+    s += str(cleaned_data_2020[i][3] * 1300000)
+    fout.write(s + "\n")
 fout.close()
-
-
-
-
-
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -10,19 +10,15 @@ output_neuron_1_targets = [1.0, 0.0]
 output_neuron_2_targets = [0.0, 1.0]
 
 
-
-# hidden_neuron_1 = Neuron(neuron_inputs, neuron_weights, Random.random())
-# hidden_neuron_2 = Neuron(neuron_inputs, neuron_weights, Random.random())
-
-
 hidden_neuron_1 = HiddenNeuron(neuron_inputs_Set1, 0, 0.01)
 hidden_neuron_2 = HiddenNeuron(neuron_inputs_Set1, 0, 0.01)
 
 output_neuron_1 = OutputNeuron([hidden_neuron_1.sigmoid(), hidden_neuron_2.sigmoid()], 0, 0.01, 1.0)
 output_neuron_2 = OutputNeuron([hidden_neuron_1.sigmoid(), hidden_neuron_2.sigmoid()], 0, 0.01, 0.0)
 
-#do stuff
-for _ in range(0,100000):
+#training the data
+epochs = 100000
+for _ in range(0,epochs):
 	for i in range(0,2):
 		output_neuron_1.target = output_neuron_1_targets[i]
 		output_neuron_2.target = output_neuron_2_targets[i]
@@ -83,62 +79,6 @@ for _ in range(0,100000):
 		for i in range(0, len(new_weights_h2)):
 			hidden_neuron_2.weights[i] = new_weights_h2[i]
 
-
-#do stuff
-# for _ in range(0,100000):
-# 	#outputs of the output neurons
-# 	output_neuron_1.inputs = [hidden_neuron_1.sigmoid(), hidden_neuron_2.sigmoid()]
-# 	output_neuron_2.inputs = [hidden_neuron_1.sigmoid(), hidden_neuron_2.sigmoid()]
-
-# 	out_output_neuron_1 = output_neuron_1.sigmoid()
-# 	out_output_neuron_2 = output_neuron_2.sigmoid()
-
-# 	print(out_output_neuron_1, out_output_neuron_2)
-
-# 	#error of each output neuron
-# 	error_1 = 0.5 * (output_neuron_1.target - out_output_neuron_1)**2
-# 	error_2 = 0.5 * (output_neuron_2.target - out_output_neuron_2)**2
-
-# 	#total error
-# 	error_total = error_1 + error_2
-
-# 	new_weights = []
-
-# 	new_weights_o1 = []
-# 	for i in range(0,len(output_neuron_1.inputs)):
-# 		new_weights_o1.append(output_neuron_1.get_new_weight(i, error_total))
-# 	new_weights.append(new_weights_o1)
-
-# 	new_weights_o2 = []
-# 	for i in range(0,len(output_neuron_2.inputs)):
-# 		new_weights_o2.append(output_neuron_2.get_new_weight(i, error_total))
-# 	new_weights.append(new_weights_o2)
-
-# 	new_weights_h1 = []
-# 	for i in range(0,len(hidden_neuron_1.inputs)):
-# 		new_weights_h1.append(hidden_neuron_1.get_new_weight(i, error_total, out_output_neuron_1, out_output_neuron_2, output_neuron_1.target, output_neuron_2.target, output_neuron_1.weights, output_neuron_2.weights))
-# 	new_weights.append(new_weights_h1)
-
-# 	new_weights_h2 = []
-# 	for i in range(0,len(hidden_neuron_2.inputs)):
-# 		new_weights_h2.append(hidden_neuron_2.get_new_weight(i, error_total, out_output_neuron_1, out_output_neuron_2, output_neuron_1.target, output_neuron_2.target, output_neuron_1.weights, output_neuron_2.weights))
-# 	new_weights.append(new_weights_h2)
-
-
-# 	#reassigning weights
-# 	for i in range(0, len(new_weights_o1)):
-# 		output_neuron_1.weights[i] = new_weights_o1[i]
-
-# 	for i in range(0, len(new_weights_o2)):
-# 		output_neuron_2.weights[i] = new_weights_o2[i]
-
-# 	for i in range(0, len(new_weights_h1)):
-# 		hidden_neuron_1.weights[i] = new_weights_h1[i]
-
-# 	for i in range(0, len(new_weights_h2)):
-# 		hidden_neuron_2.weights[i] = new_weights_h2[i]
-
-
 hidden_neuron_1.inputs = neuron_inputs_Set1
 hidden_neuron_2.inputs = neuron_inputs_Set1
 
@@ -147,5 +87,3 @@ output_neuron_2.inputs = [hidden_neuron_1.sigmoid(), hidden_neuron_2.sigmoid()]
 
 print(output_neuron_1.sigmoid())
 print(output_neuron_2.sigmoid())
-
-# print(output_neuron_1.sigmoid(), output_neuron_2.sigmoid(), sep=" ")
